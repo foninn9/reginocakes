e38e008afedcf04976a02fd33516f9ed90a3c0aa
SebastianDOS <sebastianontiverossepulveda@gmail.com>
2020-12-10 10:48:06 -0700

    a ver

--- sesion.php
+++ sesion.php
@@ -2,14 +2,11 @@
 include ('conection.php');
 $correos = utf8_decode($_POST['emails']);
 $contrasenas = utf8_decode($_POST['Passwords']);
-session_start();
-$_SESSION['email']=$correos;
 $consult= "SELECT * FROM Clientes WHERE Correo='$correos' and Contrasena='$contrasenas'";
 $Resul=mysqli_query($conn, $consult);
-$filas=mysqli_num_rows($Resul);
 
-if($filas){
-  
+
+if($fila=mysqli_num_rows($Resul)){
     header("location:Cakes.html");
     mysqli_close($conexion);
 }else{
